fafa406de59108ea40d7dbfacf6f79434bf98cd1
Soymaestro <112667093+Soymaestro@users.noreply.github.com>
2024-02-24 11:36:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("login-form").addEventListener("submit", function(event){
+    event.preventDefault(); // Prevenir el envío del formulario por defecto
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username === "lavaderola86" && password === "administrador"){
+        // Si las credenciales son correctas, redirigir al usuario a la página deseada
+        window.location.href = "REGISTROLAV2023.html";
+    } else {
+        alert("NOMBRE DE USUARIO O CONTRASEÑA INCORRECTA. Inténtalo de nuevo.");
+    }
+});
